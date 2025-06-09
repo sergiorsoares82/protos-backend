fe222b5344c1e8648900b554f02b9dd37b1c321a
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-06-09 12:05:54 -0300

    feat: add initial Express server implementation with TypeScript

---
+++ dist/index.mjs
@@ -0,0 +1,11 @@
+// src/index.ts
+import express from "express";
+var app = express();
+var port = process.env.PORT || 3e3;
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello from Express + TypeScript!");
+});
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
